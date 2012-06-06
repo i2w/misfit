3546783ba007d6de80e488dfce6a2479d7fea608
Ian White <ian.w.white@gmail.com>
2012-06-06 10:54:06 +0100

    Version 0.0.2 * Support for the way rspec instantiates errors in stubs * Support for instantiation without error messages

--- lib/misfit.rb
+++ lib/misfit.rb
@@ -3,15 +3,15 @@ require 'active_support/core_ext'
 
 module Misfit
   extend ActiveSupport::Concern
-  
+
   included do inherit_from_misfit end
-  
+
   def self.exception_class
     StandardError
   end
-  
+
   attr_accessor :data
-  
+
   module ClassMethods
     # when called with an exception, makes the exception a <self>
     # when called with a block, any exceptions raised in the block will be wrapped as <self>
@@ -22,20 +22,20 @@ module Misfit
         wrap_exception *args
       end
     end
-    
-    def new message, data = nil, *backtrace
+
+    def new message = nil, data = nil, *backtrace
       wrap_exception exception_class.exception(message, *backtrace), data
     end
-    
-    def exception message, *args
+
+    def exception message = nil, *args
       new message, nil, *args
     end
-    
+
     def exception_class klass = nil
       @exception_class = klass if klass
       @exception_class
     end
-    
+
   private
     def wrap_exception exception, data = nil
       exception.tap do |e|
@@ -43,13 +43,13 @@ module Misfit
         e.data = data if data
       end
     end
-    
+
     def wrap_block
       yield
     rescue Exception => exception
       raise wrap_exception(exception)
     end
-    
+
     # when an exception module is included, set up this module
     # as an exception module itself, and 'inherit' the exception_class
     def inherit_from_misfit
@@ -57,5 +57,10 @@ module Misfit
       exception_class ancestors[1].exception_class
       included do inherit_from_misfit end
     end
+  end
+
+  # this is here solely because of the way rspec instantiates exception objects in stubs
+  def initialize *args
+    super
   end
 end
--- spec/misfit_spec.rb
+++ spec/misfit_spec.rb
@@ -9,21 +9,21 @@ module MisfitSpec
   module Error
     include Misfit
   end
-  
+
   module FooError
     include Error
     exception_class RuntimeError
   end
-  
+
   module FazError
     include FooError
     exception_class IOError
   end
-  
+
   module BarError
     include FazError
   end
-  
+
   describe Misfit do
     describe 'Error (includes Misfit)' do
       subject { exception_module }
@@ -33,27 +33,35 @@ module MisfitSpec
       let(:data)             { 'exception data' }
 
       describe 'standard idioms' do
+        it 'raise <exception_module> should raise error with no message' do
+          expect { raise exception_module }.to raise_error(exception_module)
+        end
+
+        it 'exception_module.exception should return error with no message' do
+          exception_module.exception.should be_a(exception_module)
+        end
+
         describe 'raise <exception_module>, <message>' do
           subject { raise exception_module, message }
 
           it { expect{ subject }.to raise_error(StandardError, message) }
           it { expect{ subject }.to raise_error(exception_module, message) }
         end
-      
+
         describe 'raise <exception_module>.new(<message>)' do
           subject { raise exception_module.new(message) }
 
           it { expect{ subject }.to raise_error(StandardError, message) }
           it { expect{ subject }.to raise_error(exception_module, message) }
         end
-      
+
         describe 'raise <exception_module>.exception(<message>)' do
           subject { raise exception_module.exception(message) }
 
           it { expect{ subject }.to raise_error(StandardError, message) }
           it { expect{ subject }.to raise_error(exception_module, message) }
         end
-        
+
         describe 'FooError (includes Error, exception_class RuntimeError)' do
           let(:exception_module) { FooError }
 
@@ -65,7 +73,7 @@ module MisfitSpec
             it { expect{ subject }.to raise_error(RuntimeError, message) }
           end
         end
-        
+
         describe 'FazError (includes FooError, exception_class IOError)' do
           let(:exception_module) { FazError }
 
@@ -79,7 +87,7 @@ module MisfitSpec
             it { expect{ subject }.to raise_error(IOError, message) }
           end
         end
-        
+
         describe 'BarError (includes FazError)' do
           let(:exception_module) { BarError }
 
@@ -93,12 +101,20 @@ module MisfitSpec
             it { expect{ subject }.to raise_error(IOError, message) }
           end
         end
+
+        describe "used in specs" do
+          it "should work with stubs" do
+            object = Object.new
+            object.stub(:foo).and_raise(Error)
+            expect { object.foo }.to raise_error(Error)
+          end
+        end
       end
-      
+
       describe 'adding data' do
         describe 'raise <exception_module>.new <message>, <data>' do
           subject { raise exception_module.new(message, data) }
-                  
+
           it "should have data added to the exception" do
             begin
               subject
@@ -108,28 +124,28 @@ module MisfitSpec
           end
         end
       end
-      
+
       describe '#wrap' do
         describe 'RuntimeError.new(<message>)' do
           subject { exception_module.wrap RuntimeError.new(message) }
-          
+
           it { should be_a RuntimeError }
           it { should be_a exception_module }
           its(:message) { should == message }
         end
-        
+
         describe 'RuntimeError.new(<message>), <exception data>' do
           subject { exception_module.wrap RuntimeError.new(message), data }
-          
+
           it { should be_a RuntimeError }
           it { should be_a exception_module }
           its(:message) { should == message }
           its(:data) { should == data }
         end
-        
+
         describe '{ some_error_ridden_code! }' do
           subject { exception_module.wrap do some_error_ridden_code! end }
-            
+
           it { expect{ subject }.to raise_error(NoMethodError, /some_error_ridden_code!/) }
           it { expect{ subject }.to raise_error(exception_module, /some_error_ridden_code!/) }
         end
